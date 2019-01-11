b835a01b3fc548e7e4865bf179610d8be7c8ce6e
raden93 <rademax93@gmail.com>
2019-01-11 16:18:31 +0100

    fix baleofjute block and added axes

--- src/main/java/com/raden93/veganlife/block/jute/BaleOfJuteBlock.java
+++ src/main/java/com/raden93/veganlife/block/jute/BaleOfJuteBlock.java
@@ -3,12 +3,15 @@ package com.raden93.veganlife.block.jute;
 import com.raden93.veganlife.init.VeganLifeTabs;
 
 import net.minecraft.block.Block;
+import net.minecraft.block.BlockRotatedPillar;
 import net.minecraft.block.material.Material;
+import net.minecraft.block.properties.PropertyInteger;
+import net.minecraft.util.EnumFacing;
 
-public class BaleOfJuteBlock extends Block {
-
+public class BaleOfJuteBlock extends BlockRotatedPillar {
 	public BaleOfJuteBlock() {
 		super(Material.GROUND);
+		this.setDefaultState(this.blockState.getBaseState().withProperty(AXIS, EnumFacing.Axis.Y));
 		setCreativeTab(VeganLifeTabs.veganLifeTab);
 	}
 }
