fae0cf606346055be16e83bf917566dd39d385a8
Max Soest <maxsoest@gmail.com>
2019-01-11 10:45:55 +0100

    change project strucute and added burlap item

---
+++ src/main/java/com/raden93/veganlife/item/jute/JuteSeedsItem.java
@@ -0,0 +1,12 @@
+package com.raden93.veganlife.item.jute;
+
+import com.raden93.veganlife.init.VeganLifeTabs;
+
+import net.minecraft.item.Item;
+
+public class JuteSeedsItem extends Item {
+
+	public JuteSeedsItem() {
+		setCreativeTab(VeganLifeTabs.veganLifeTab);
+	}
+}
