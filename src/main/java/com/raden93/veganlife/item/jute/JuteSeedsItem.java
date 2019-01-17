f38f1f38001c1118fb882c9fd2cbbb918cb64aa6
raden93 <rademax93@gmail.com>
2019-01-17 16:34:23 +0100

    added textures, blockstate, and classes

--- src/main/java/com/raden93/veganlife/item/jute/JuteSeedsItem.java
+++ src/main/java/com/raden93/veganlife/item/jute/JuteSeedsItem.java
@@ -1,12 +1,24 @@
 package com.raden93.veganlife.item.jute;
 
+import com.raden93.veganlife.init.VeganLifeBlocks;
 import com.raden93.veganlife.init.VeganLifeTabs;
 
 import net.minecraft.item.Item;
+import net.minecraft.item.ItemSeeds;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.IBlockAccess;
+import net.minecraftforge.common.EnumPlantType;
 
-public class JuteSeedsItem extends Item {
-
+public class JuteSeedsItem extends ItemSeeds {
+	
 	public JuteSeedsItem() {
+		super(VeganLifeBlocks.jute_crop_block, null);
 		setCreativeTab(VeganLifeTabs.veganLifeTab);
+	}
+	
+	@Override
+	public EnumPlantType getPlantType(IBlockAccess world, BlockPos pos)
+	{
+		return EnumPlantType.Plains;
 	}
 }
