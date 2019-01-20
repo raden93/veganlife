67b168fbf481bb9d084f37ea6c90d483d5048819
Max Soest <maxsoest@gmail.com>
2019-01-20 17:53:38 +0100

    jungle leaves drops kapok tuft now

--- src/main/java/com/raden93/veganlife/init/VeganLifeDropChanger.java
+++ src/main/java/com/raden93/veganlife/init/VeganLifeDropChanger.java
@@ -1,11 +1,14 @@
 package com.raden93.veganlife.init;
 
+import java.util.List;
 import java.util.Random;
-
 import com.raden93.veganlife.VeganLifeMod;
-
+import com.raden93.veganlife.item.wool.KapokTuftItem;
 import net.minecraft.block.Block;
 import net.minecraft.block.BlockDoublePlant;
+import net.minecraft.block.BlockLeaves;
+import net.minecraft.block.BlockOldLeaf;
+import net.minecraft.block.BlockPlanks;
 import net.minecraft.block.state.IBlockState;
 import net.minecraft.init.Blocks;
 import net.minecraft.item.ItemShears;
@@ -19,7 +22,13 @@ public class VeganLifeDropChanger {
 	public void onHarvestBlock(HarvestDropsEvent event)
 	{
 		if(!this.userUseShears(event)) {
-			this.dropJuteFromFerns(event);
+			IBlockState state = event.getState();
+			Block block = state.getBlock();
+			Random random = new Random();
+			List<ItemStack> drops = event.getDrops();
+			
+			this.dropJuteFromFerns(block, state, random, drops);
+			this.dropKapokFromJungle(block, state, random, drops);
 		}
 	}
 	
@@ -27,13 +36,17 @@ public class VeganLifeDropChanger {
 	 * Fern now drops 1-3 Jute Stalks
 	 * @param event
 	 */
-	private void dropJuteFromFerns(HarvestDropsEvent event) {
-		IBlockState state = event.getState();
-		Block block = state.getBlock();
-		Random rnd = new Random();
-		
+	private void dropJuteFromFerns(Block block, IBlockState state, Random random, List<ItemStack> drops) {
 		if(block == Blocks.DOUBLE_PLANT && state.getValue(BlockDoublePlant.VARIANT) == BlockDoublePlant.EnumPlantType.FERN) {
-			event.getDrops().add(new ItemStack(VeganLifeItems.jute_stalk_item, rnd.nextInt(2)+1));
+			drops.add(new ItemStack(VeganLifeItems.jute_stalk_item, random.nextInt(2)+1));
+		}
+	}
+	
+	private void dropKapokFromJungle(Block block, IBlockState state, Random random, List<ItemStack> drops) {
+		if(block == Blocks.LEAVES && state.getValue(BlockOldLeaf.VARIANT) == BlockPlanks.EnumType.JUNGLE) {
+			if(random.nextFloat() < 0.07) {
+				drops.add(new ItemStack(VeganLifeItems.kapok_tuft_item, 1));
+			}
 		}
 	}
 	
