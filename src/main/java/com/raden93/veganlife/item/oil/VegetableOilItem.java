4ac34a353f06520761a4108fa633e9ca75d005b8
raden93 <rademax93@gmail.com>
2019-01-24 16:23:40 +0100

    try to add vegetable oil recipe

--- src/main/java/com/raden93/veganlife/item/oil/VegetableOilItem.java
+++ src/main/java/com/raden93/veganlife/item/oil/VegetableOilItem.java
@@ -2,12 +2,14 @@ package com.raden93.veganlife.item.oil;
 
 import com.raden93.veganlife.init.VeganLifeTabs;
 
+import net.minecraft.init.Items;
 import net.minecraft.item.Item;
 
 public class VegetableOilItem extends Item {
 	
 	public VegetableOilItem() {
 		setCreativeTab(VeganLifeTabs.veganLifeTab);
+		setContainerItem(Items.GLASS_BOTTLE);
 	}
 
 }
