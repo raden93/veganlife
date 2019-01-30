cc33f68870ab7092b45006b9b68dfad19937b06b
Max Soest <rademax93@gmail.com>
2019-01-30 22:09:32 +0100

    added two classes for enderrift

---
+++ src/main/java/com/raden93/veganlife/block/enderperl/EnderRiftBlock.java
@@ -0,0 +1,12 @@
+package com.raden93.veganlife.block.enderperl;
+
+import net.minecraft.block.Block;
+import net.minecraft.block.material.Material;
+
+public class EnderRiftBlock extends Block {
+
+	public EnderRiftBlock(Material materialIn) {
+		super(materialIn);
+	}
+
+}
