5fdc37b80f803fdbf6141ad1b90f03c030a9dc6b
Max Soest <rademax93@gmail.com>
2019-02-11 16:42:49 +0100

    flying end block push player to the air

--- src/main/java/com/raden93/veganlife/block/enderperl/FlyingEndBlock.java
+++ src/main/java/com/raden93/veganlife/block/enderperl/FlyingEndBlock.java
@@ -41,23 +41,18 @@ public class FlyingEndBlock extends Block {
     }
 	
 	public void updateTick(World worldIn, BlockPos pos, IBlockState state, Random rand) {
-		if(pos.getY() < 100) {
-			AxisAlignedBB box = new AxisAlignedBB(pos, pos.up());
-			List<EntityPlayer> list = worldIn.getEntitiesWithinAABB(EntityPlayer.class, box);
-			for(EntityPlayer player: list) {
-				player.move(MoverType.PISTON, 0, 100, 0);
+		if(worldIn.isAirBlock(pos.up())) {			
+			if(pos.getY() < 240) {			
+				AxisAlignedBB box = new AxisAlignedBB(pos.getX()-1, pos.getY()-1, pos.getZ()-1, pos.getX()+1, pos.getY() + 2, pos.getZ()+1 );
+				List<EntityPlayer> list = worldIn.getEntitiesWithinAABB(EntityPlayer.class, box);
+				for(EntityPlayer player: list) {
+					player.move(MoverType.PLAYER, 0, 1, 0);
+				}
+								
+				BlockSnapshot snapshot = BlockSnapshot.getBlockSnapshot(worldIn, pos);
+				snapshot.restoreToLocation(worldIn, pos.up(), true, true);	
 			}
-						
-			BlockSnapshot snapshot = BlockSnapshot.getBlockSnapshot(worldIn, pos);
-			snapshot.restoreToLocation(worldIn, pos.up(), false, true);	
+			worldIn.setBlockToAir(pos); 
 		}
-		worldIn.setBlockToAir(pos);
-		/*
-		 if (!worldIn.isRemote) {		 
-			 System.out.println("run");
-			 FlyingVeganBlockEntity entityfallingblock = new FlyingVeganBlockEntity(worldIn,(double)pos.getX() + 0.5D, (double)pos.getY(), (double)pos.getZ() + 0.5D, worldIn.getBlockState(pos));
-             worldIn.spawnEntity(entityfallingblock);
-	     }*/
-		 
 	}
 }
